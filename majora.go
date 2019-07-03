afe57e252e7d8a809593ba3981adf3d0b20e3515
Igor Franca <igor.reis@inmetrics.com.br>
2019-07-03 07:20:59 -0300

    feat(Runtime): Added runtime and VM initialization function.

---
+++ majora.go
@@ -0,0 +1,72 @@
+package majora
+
+import (
+	luaJSON "layeh.com/gopher-json"
+	"github.com/yuin/gopher-lua"
+
+	"fmt"
+)
+
+func loadVM() (vm *lua.LState) {
+	vm = lua.NewState()
+
+	vm.PreloadModule("json", luaJSON.Loader)
+
+	fileModule := `
+		local fs = {}
+
+		function fs.read_file(filename) 
+			local file = io.open(filename, "r")
+
+			local content = file:read("*all")
+			file:close()
+
+			return content
+		end
+
+		return fs
+	`
+
+	mod, err := vm.LoadString(fileModule)
+
+	if err != nil {
+		panic(err)
+	}
+
+	loadedIndex := vm.GetField(vm.GetField(vm.Get(lua.EnvironIndex), "package"), "preload")
+
+	vm.SetField(loadedIndex, "fs", mod)
+	return
+}
+
+// RunScript function runs the given Lua script string
+// using a predefined environment and VM.
+// WARNING: This function does not handle errors.
+// @TODO: Handle custom errors.
+func RunScript(script string) (parserJSON []byte) {
+	vm := loadVM()
+	defer vm.Close()
+
+	if err := vm.DoString(script); err != nil {
+    	panic(err)
+	}
+
+	parserJSON = []byte(vm.Get(-1).(lua.LString))
+	return
+}
+
+// RunFile runs a given Lua script file
+// using the same context as the RunScript function.
+// WARNING: This function does not handle errors.
+// @TODO: Handle custom errors.
+func RunFile(filename string) (parserJSON []byte) {
+	vm := loadVM()
+	defer vm.Close()
+
+	if err := vm.DoFile(filename); err != nil {
+		panic(err)
+	}
+
+	parserJSON = []byte(vm.Get(-1).(lua.LString))
+	return
+}
